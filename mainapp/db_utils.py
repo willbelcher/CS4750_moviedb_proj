e6e1426cad373835bf877a1dfe6e818d57f54a7c
willbelcher <eaglebelcher@me.com>
2024-04-08 21:42:53 -0400

    urls and db handler skeleton

---
+++ mainapp/db_utils.py
@@ -0,0 +1,5 @@
+from django.db import connection
+
+# add raw sql statements here
+# https://docs.djangoproject.com/en/5.0/topics/db/sql/#executing-custom-sql-directly
+# with connection.cursor() as cursor: cursor.execute(...)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+
+    return render(request, 'home.html')
+
+
+def login_view(request):
+
+    return render(request)
+
+
+def logout_view(request):
+
+    return render(request)
+
