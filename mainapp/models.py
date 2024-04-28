cad87a2403c391b617386d9cc06ed09d3d97768e
matthew-cahill <msc3jk@virginia.edu>
2024-04-28 19:29:39 -0400

    added siteadmin model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class SiteAdmin(models.Model):
+    user = models.ForeignKey(User, related_name='siteadmin', on_delete=models.CASCADE)
