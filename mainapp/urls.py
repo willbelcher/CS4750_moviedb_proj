03a983cbb7163b7ff6c309ed4435f9d6a3e79fbf
willbelcher <eaglebelcher@me.com>
2024-04-29 00:39:14 -0400

    movie search and typos

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 app_name = "mainapp"
 urlpatterns = [
-    path('', views.home, name='myreviews'),
-    path('moviesearch/', views.movie_search, name='moviesearch'),
+    path('', views.home, name='home'),
+    path('movie-search/', views.search_movies, name='movie_search'),
+    path('user-search/', views.search_users, name='user_search'),
     path('logout/', views.handle_logout, name='logout'),
     path('login/', views.user_login, name='login'),
-    path('create-account', views.create_account, name='create_account'), # adds to db
+    path('create-account/', views.create_account, name='create_account'),
+    path('add-watchlist/', views.add_watchlist, name='add_watchlist'),
 ]
